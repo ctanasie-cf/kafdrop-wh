f2228e1f0cfccc73eead978bc28f22be5d8e2a9e
ctanasie-cf <77156179+ctanasie-cf@users.noreply.github.com>
2021-01-08 15:24:18 +0200

    update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -jar ./target/kafdrop-3.28.0-SNAPSHOT.jar --kafka.brokerConnect=kafka-service:9092 --schemaregistry.connect=http://schema-registry-service:8081 --message.format=AVRO --key.format=DEFAULT
+java -jar ./target/kafdrop-3.28.0-SNAPSHOT.jar --kafka.brokerConnect=kafka-service:9092 --schemaregistry.connect=http://schema-registry-service:8081 --message.format=DEFAULT
